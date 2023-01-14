92416b8b42b018133fd3b68e2e63ab1022928d87
viju-dev <vijaydorugade20@gmail.com>
2023-01-15 01:30:42 +0530

    files are uploaded to repo

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly Obj = new RWOnly();
+//        Obj.name = "Vijay";
+//        'name' has private access so cant set name directly
+//        we can print error using printStackTrace(),getMessage(),toString() inside try-catch block
+        Obj.setName("Vijay");
+        System.out.println(Obj.getName());
+
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+
 }
